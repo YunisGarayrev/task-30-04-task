35e0e4c17ad9dc351a8d3714813018e29264a6d4
YunisGarayev <79d2mw0@code.edu.az>
2023-04-30 12:11:19 -0700

    task 30-04

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+// birinci usul
+
+
+// var textInput = document.querySelector('input[type="text"]');
+// var colorInput = document.querySelector('input[type="color"]');
+// var btn = document.querySelector("button");
+// btn.addEventListener("click", function () {
+//     document.body.style.color = colorInput.value;
+//     var text = textInput.value;
+//     var p = document.createElement("p");
+//     p.textContent = text;
+//     document.body.appendChild(p);
+//     textInput.value = "";
+// });
+
+
+
+
+// ikinci usul
+
+// var textInput = document.querySelector('input[type="text"]');
+// var colorInput = document.querySelector('input[type="color"]');
+// var btn = document.querySelector("button");
+// btn.onclick = function() {
+//     document.body.style.color = colorInput.value;
+//     var text = textInput.value;
+//     var p = document.createElement("p");
+//     p.textContent = text;
+//     document.body.appendChild(p);
+//     textInput.value = "";
+// }
+
+
